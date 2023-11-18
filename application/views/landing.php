20fc8e147756cfd372c07a54f243032d40d2d263
kamseea <138553231+kamseea@users.noreply.github.com>
2023-11-19 03:48:00 +0800

    Update landing.php

--- application/views/landing.php
+++ application/views/landing.php
@@ -1,9 +1,77 @@
-
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Cover Page</title>
+    <style>
+        .bg {
+            background-image: url('https://live.staticflickr.com/8546/8668286838_dbfe408306_b.jpg');
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            color: white; 
+            text-align: center;
+            padding: 200px; 
+            margin: 0; 
+           
+           
+        }
+
+    
+        h1 {
+            font-size: 2em;
+            margin-bottom: 20px; /
+        }
+
+        h2{
+            font-weight:700;
+            color:#204c37;
+        }
+
+
+        .row-container {
+            display: flex;
+            flex-direction: row;
+            justify-content: space-around;
+            align-items: center;
+            padding: 30px;
+            text-align: center;
+        }
+
+        .column {
+            flex: 1;
+            background-color:white;
+            border-radius: 10px;
+        }
+
+        .bot{
+            padding-bottom:80px;
+        }
+
+        .t0p{
+            padding-top:25px;
+        }
+    </style>
 </head>
 <body>
-    LOREM IPSUM
+    <div class="bg">
+    <h1>Welcome to<p>Ninoy Aquino Parks and Wildlife Center</p></h1>
+    </div>
+
+    <div class="row-container">
+        <div class="column">
+        <h2 class="t0p">222</h2>
+        <p class="bot">DATA</p>
+        </div>
+        <div class="column">
+            <h2 class="t0p">100</h2>
+            <p class="bot">ACHIEVEMENTS</p>
+        </div>
+        <div class="column">
+            <h2 class="t0p">143</h2>
+            <p class="bot">PROGRAMS</p>
+        </div>
 </body>
+
+</html>
