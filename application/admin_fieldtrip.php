a4ed98919b6901e01977fed9cd3d66be861fed2c
biancafamatiga <91246724+biancafamatiga@users.noreply.github.com>
2023-11-18 23:41:21 +0800

    Add files via upload

---
+++ application/admin_fieldtrip.php
@@ -0,0 +1,51 @@
+
+
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<?php require_once('admin_nav.php');?>
+<div class ="container">
+<div class="admin_search">
+      <form class="d-flex" role="search" form action="http://localhost/quads/index.php/napwc/search_fieldtrip" method = "post">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="search_key">
+        <button class="btn btn-outline-success" type="submit" value="search" name="search">Search</button>
+      </form>
+      </div>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Name of School/Organization</th>
+      <th scope="col">Contact Person</th>
+      <th scope="col">Email Address</th>
+      <th scope="col">Phone Number</th>
+      <th scope="col">Date</th>
+      <th scope="col">Time</th>
+      <th scope="col">No. of Participants</th>
+      <th scope="col">Grade Level</th>
+      <th scope="col">Requests</th>
+    </tr>
+  </thead>
+  <tbody>
+    <!--loop-->
+    <?php
+    for ($i = 0; $i < sizeof($item); $i++){
+    ?>
+    <tr>
+        
+        <td><?php echo urldecode($item[$i] -> id);?></td>
+        <td><?php echo urldecode($item[$i] -> name);?></td>
+        <td><?php echo urldecode($item[$i] -> contactperson);?></td>
+        <td><?php echo urldecode($item[$i] -> email);?></td>
+        <td><?php echo urldecode($item[$i] -> phone);?></td>
+        <td><?php echo urldecode($item[$i] -> date);?></td>
+        <td><?php echo urldecode($item[$i] -> time);?></td>
+        <td><?php echo urldecode($item[$i] -> participants);?></td>
+        <td><?php echo urldecode($item[$i] -> gradelevel);?></td>
+        <td><?php echo urldecode($item[$i] -> request);?></td>
+        <td><button type="button" class="delete-btn"><a href= "<?php echo 'http://localhost/quads/index.php/napwc/delete_fieldtrip/' . $item[$i] -> id; ?>" class="delete-txt"> Delete </img> </a></td>
+    </tr>
+    <?php } ?>
+  </tbody>
+</table>
+</div>
---
+++ application/plan.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+     
+        h1{
+            color:#204c37;
+            text-align:center;
+            font-weight:700;
+            padding-top:70px;
+        }
+
+        h3{
+            text-align:center;
+            padding-bottom:20px;
+        }
+        .c{
+            text-align:center;
+        }
+        
+        .button-link {
+            display: block;
+            padding: 10px 20px;
+            margin: 10px auto; 
+            text-align: center;
+            text-decoration: none;
+            color: #fff;
+            background-color: #204c37;
+            border-radius: 5px;
+            max-width:400px;
+        }
+
+        .button-link:hover {
+            background-color: grey;
+        }
+
+        </style>
+</head>
+<body>
+    <h1> Plan your Visit </h1>
+    <h3> Make a Reservation </h3>
+    <div class="c">
+    <a href="http://localhost/quads/index.php/napwc/fieldtrip" class="button-link">Field Trip</a>
+    </div>
+    <div>
+    <a href="http://localhost/quads/index.php/napwc/appointment" class="button-link">Appointment</a>
+    </div>
+    <div>
+    <a href="http://localhost/quads/index.php/napwc/bookfacility" class="button-link">Book a Facility</a>
+    </div>
+
+</body>
+</html>
