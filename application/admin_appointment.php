b04e141996441ea4ec4c981fff3014e8499fd10e
kamseea <138553231+kamseea@users.noreply.github.com>
2023-11-19 21:00:40 +0800

    Update admin_appointment.php

--- application/admin_appointment.php
+++ application/admin_appointment.php
@@ -1,50 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <style>
+     
+
+        .admin_search {
+            margin-bottom: 20px;
+        }
+    </style>
 </head>
-<?php require_once('admin_nav.php');?>
+
 <body>
-<div class ="container">
-<div class="admin_search">
-      <form class="d-flex" role="search" form action="http://localhost/quads/index.php/napwc/search_appointment" method = "post">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="search_key">
-        <button class="btn btn-outline-success" type="submit" value="search" name="search">Search</button>
-      </form>
-      </div>
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">ID</th>
-      <th scope="col">Name</th>
-      <th scope="col">Organization/Company/School</th>
-      <th scope="col">Email Address</th>
-      <th scope="col">Phone Number</th>
-      <th scope="col">Date</th>
-      <th scope="col">Time</th>
-      <th scope="col">Purpose</th>
-      <th scope="col">Questions/Inquiries</th>
-    </tr>
-  </thead>
-  <tbody>
-    <!--loop-->
-    <?php
-    for ($i = 0; $i < sizeof($item); $i++){
-    ?>
-    <tr>
-        <td><?php echo urldecode($item[$i] -> id);?></td>
-        <td><?php echo urldecode($item[$i] -> name);?></td>
-        <td><?php echo urldecode($item[$i] -> organization);?></td>
-        <td><?php echo urldecode($item[$i] -> email);?></td>
-        <td><?php echo urldecode($item[$i] -> phone);?></td>
-        <td><?php echo urldecode($item[$i] -> date);?></td>
-        <td><?php echo urldecode($item[$i] -> time);?></td>
-        <td><?php echo urldecode($item[$i] -> purpose);?></td>
-        <td><?php echo urldecode($item[$i] -> questions);?></td>
-        <td><button type="button" class="delete-btn"><a href= "<?php echo 'http://localhost/quads/index.php/napwc/delete_appointment/' . $item[$i] -> id; ?>" class="delete-txt"> Delete </img> </a></td>
+    <?php require_once('admin_nav.php'); ?>
+    <div class="container">
+        <div class="admin_search">
+            <form class="d-flex" role="search" form action="http://localhost/quads/index.php/napwc/search_appointment"
+                method="post">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"
+                    name="search_key">
+                <button class="btn btn-outline-success" type="submit" value="search" name="search">Search</button>
+            </form>
+        </div>
+        <table class="table table-striped table-bordered">
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Organization/Company/School</th>
+                    <th scope="col">Email Address</th>
+                    <th scope="col">Phone Number</th>
+                    <th scope="col">Date</th>
+                    <th scope="col">Time</th>
+                    <th scope="col">Purpose</th>
+                    <th scope="col">Questions/Inquiries</th>
+                    <th scope="col">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <!--loop-->
+                <?php
+                for ($i = 0; $i < sizeof($item); $i++) {
+                ?>
+                    <tr>
+                        <td><?php echo urldecode($item[$i]->id); ?></td>
+                        <td><?php echo urldecode($item[$i]->name); ?></td>
+                        <td><?php echo urldecode($item[$i]->organization); ?></td>
+                        <td><?php echo urldecode($item[$i]->email); ?></td>
+                        <td><?php echo urldecode($item[$i]->phone); ?></td>
+                        <td><?php echo urldecode($item[$i]->date); ?></td>
+                        <td><?php echo urldecode($item[$i]->time); ?></td>
+                        <td><?php echo urldecode($item[$i]->purpose); ?></td>
+                        <td><?php echo urldecode($item[$i]->questions); ?></td>
+                        <td>
+                            <a href="<?php echo 'http://localhost/quads/index.php/napwc/delete_appointment/' . $item[$i]->id; ?>"
+                                class="btn btn-danger btn-sm">Delete</a>
+                        </td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
 
-    </tr>
-    <?php } ?>
-  </tbody>
-</table>
-</div>
-    </body>
+    <!-- Bootstrap JS and any additional scripts -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-rLQU8wfZYg6aRq63t1N5/IOZVeV6SjlF8eZuAgP4W1qzDqAryuXVMO8aDc80Jgj9" crossorigin="anonymous">
+    </script>
+</body>
 
+</html>
